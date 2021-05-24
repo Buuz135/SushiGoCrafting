2f578379d07a78d187fef81c5ba965d66a85a5a9
Buuz135 <buuz135@hotmail.com>
2021-05-24 16:54:19 +0200

    Added missing localizations for entities

--- src/main/java/com/buuz135/sushigocrafting/datagen/SushiLangProvider.java
+++ src/main/java/com/buuz135/sushigocrafting/datagen/SushiLangProvider.java
@@ -16,6 +16,8 @@ public class SushiLangProvider extends LanguageProvider {
     @Override
     protected void addTranslations() {
         add("itemGroup.sushigocrafting", "Sushi Go Crafting");
+        add("entity.sushigocrafting.shrimp", "Shrimp");
+        add("entity.sushigocrafting.tuna", "Tuna");
         add("text.sushigocrafting.book.title", "Becoming an Itamae (Sushi Go Crafting Manual)");
         SushiContent.Items.REGISTRY.getEntries().stream().map(RegistryObject::get).filter(item -> item instanceof BlockItem).map(item -> (BlockItem) item).forEach(blockItem -> {
             add(blockItem, WordUtils.capitalize(blockItem.getRegistryName().getPath().replaceAll("_", " ")));
